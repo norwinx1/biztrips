a46a73660572e6b4d6178ad0196757ab12c32858
Norwin Schäfer <norwin270@gmail.com>
2022-01-03 15:21:05 +0100

    added mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,97 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 //import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import {Box, FormControl, InputLabel} from "@mui/material";
 
 export default function App() {
-  const [month, setMonth] = useState("");
+    const [month, setMonth] = React.useState('');
+    const handleFilter = (event) => {
+        setMonth(event.target.value);
+    };
 
-  const { data: trips, loading: loadingTrips, error: errorTrips } = useFetch(
-    "trips"
-  );
-  const months = ["Idle", "Jan", "Feb", "March", "April", "Mai", "June"];
+    const {data: trips, loading: loadingTrips, error: errorTrips} = useFetch(
+        "trips"
+    );
+    const months = ["Idle", "Jan", "Feb", "March", "April", "Mai", "June"];
 
-  function renderTrip(t) {
-    return (
-      <div className="product" key={t.id}>
-        <figure>
-          <div>
-            <img src={"images/items/" + t.id + ".jpg"} alt="name " />
-          </div>
-          <figcaption>
-            <a href="#. . . ">{t.title}</a>
-            <div>
+    function renderTrip(t) {
+        return (
+            <div className="product" key={t.id}>
+                <figure>
+                    <div>
+                        <img src={"images/items/" + t.id + ".jpg"} alt="name "/>
+                    </div>
+                    <figcaption>
+                        <a href="#. . . ">{t.title}</a>
+                        <div>
               <span>
                 {t.startTrip[2] + "-" + t.startTrip[1] + "-" + t.startTrip[0]}
               </span>
+                        </div>
+                        <p>{t.description}</p>
+                        <div>
+                            <Button type="button" variant="contained">
+                                Add to Triplist
+                            </Button>
+                        </div>
+                    </figcaption>
+
+                </figure>
+
             </div>
-            <p>{t.description}</p>
+        );
+    }
+
+    // if month selected then filter the trips from month === month
+    const filteredTrips = month ? trips.filter((t) => t.startTrip[1] === parseInt(month)) : trips;
+
+    // if error then throw the errror
+    if (errorTrips) throw errorTrips;
+    if (loadingTrips) return <Spinner/>;
+    // shorthand for react fragment
+    return (
+        <>
+
             <div>
-              <button type="button" disabled>
-                Add to Triplist
-              </button>
+                <Header/>
+                <main>
+                    <section id="filters">
+                        <Box sx={{minWidth: 30}}>
+                            <FormControl fullWidth>
+                                <InputLabel id="month">Month</InputLabel>
+                                <Select
+                                    labelId="month"
+                                    value={month}
+                                    label="Month"
+                                    onChange={handleFilter}
+                                >
+                                    <MenuItem value={months[0]}>Januar</MenuItem>
+                                    <MenuItem value={months[1]}>Februar</MenuItem>
+                                    <MenuItem value={months[2]}>März</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Box>
+                    </section>
+                    {month && (
+                        <h2>
+                            Found {filteredTrips.length}
+                            {filteredTrips.length > 1 ? " trips" : " trip"} for the month of
+                            {" " + month}
+                        </h2>
+                    )}
+                    <section id="products">{filteredTrips.map(renderTrip)}</section>
+
+                </main>
+
             </div>
-          </figcaption>
-
-        </figure>
-
-      </div>
+            <Footer/>
+        </>
     );
-  }
-  // if month selected then filter the trips from month === month
-  const filteredTrips = month
-    ? trips.filter((t) => t.startTrip[1] === parseInt(month))
-    : trips;
-
-  // if error then throw the errror
-  if (errorTrips) throw errorTrips;
-  if (loadingTrips) return <Spinner />;
-  // shorthand for react fragment
-  return (
-    <>
-
-      <div>
-        <Header />
-        <main>
-          <section id="filters">
-            <label htmlFor="month">Filter by Month:</label>
-            <select
-              id="month"
-              value={month} // controlled component
-              onChange={(e) => {
-                //debugger;
-                setMonth(e.target.value);
-              }}
-            >
-              <option value="">All Months</option>
-              <option value="1">January</option>
-              <option value="2">February</option>
-              <option value="3">March</option>
-              <option value="4">April</option>
-              <option value="5">Mai</option>
-              <option value="6">June</option>
-            </select>
-            {month && (
-              <h2>
-                Found {filteredTrips.length}
-                {filteredTrips.length > 1 ? " trips" : " trip"} for the month of
-                {" " + months[month]}
-              </h2>
-            )}
-
-          </section>
-          <section id="products">{filteredTrips.map(renderTrip)}</section>
-
-        </main>
-
-      </div>
-      <Footer />
-    </>
-  );
 }
